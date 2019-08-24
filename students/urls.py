12d953905ba84c01c44a6996f41ad1922b2fd9ce
JimmyTron <oduorjames@hotmail.com>
2019-08-24 22:42:22 +0300

    Docs view

--- students/urls.py
+++ students/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('', views.home, name='Student'),
+    path('docs/', views.docs, name='Documentation'),
     path('profile/', views.profile, name='Profile'), 
     path('register/', views.register, name='students-registration'),
     path('login/', auth_views.LoginView.as_view(template_name='students/login.html'), name='students-login'),
