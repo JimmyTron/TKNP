dba8c08cd12913ab6ecc9f4471746fbb4cfc44d0
JimmyTron <oduorjames@hotmail.com>
2019-12-17 08:04:10 +0300

    cleanup

--- clearance/forms.py
+++ clearance/forms.py
@@ -1,10 +1,12 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from .models import Request
+from .models import Department, Request
 
 class ClearanceRequestForm(forms.ModelForm):
-    departments = forms.MultipleChoiceField
+    #departments = forms.MultipleChoiceField
+    departments = forms.ModelChoiceField(widget=forms.HiddenInput,queryset=Department.objects.all(),disabled=True)
+    student = forms.ModelChoiceField(widget=forms.HiddenInput,queryset=get_user_model,disabled=True)
 
     class Meta:
         model = Request
--- students/forms.py
+++ students/forms.py
@@ -26,5 +26,3 @@ class ProfileUpdateForm(forms.ModelForm):
 class DepartmentalClearanceForm(object):
     pass        
 
-class ClearanceForm(object):
-    pass
